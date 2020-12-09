e579a777166e6abf08f178d61a5008a5a3b47772
Dennis Schafroth <dennis.schafroth@agillic.com>
2020-12-09 17:29:41 +0100

    Objective-c example

---
+++ Examples/SnowplowSwiftCarthageDemo/SnowplowSwiftDemo/ExampleObjectiveC.m
@@ -0,0 +1,24 @@
+//
+//  TestObjectiveC.m
+//  SnowplowSwiftDemo
+//
+//  Created by Dennis Schafroth on 01/12/2020.
+//  Copyright © 2020 snowplowanalytics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AgillicSDK/AgillicSDK-Swift.h>
+
+
+int agillicTrackingExample() {
+    MobileSDK *sdk = [[MobileSDK alloc] init];
+    AgillicTracker *tracker = [sdk registerWithClientAppId:@"aClientAppID" clientAppVersion:@"1.0" solutionId:@"encodedSolutionId" userID:@"dennis.schafroth@agillic.com" pushNotificationToken:nil completionHandler: nil];
+    // On each application view to track:
+    // the AppViewEvent can be created once
+    // UUID should be constant for the application over restart, but is not exposed in Agillic Tracking.
+    // screen name is the value that is used in Agillic Condition Editor.
+    NSUUID *uuid = [[ NSUUID alloc] init];
+    AppViewEvent *event = [[AppViewEvent alloc] init: [uuid UUIDString] screenName:@"ios_protocol://iosapp/metrics/2" type:nil previousScreenId:nil];
+    [tracker track:event];
+}
+
